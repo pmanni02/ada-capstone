be4e890ae3e3c5674b810691b02b8e131eb5ef94
Phoebe <phoebe.manning8@gmail.com>
2018-07-03 21:18:45 -0700

    created pojo classes for image data received from api

---
+++ app/src/main/java/com/example/phoebemanning/capstone/Models/Image_Models/ImageData.java
@@ -0,0 +1,16 @@
+package com.example.phoebemanning.capstone.Models.Image_Models;
+
+public class ImageData {
+
+    Items[] items;
+
+    public Items[] getItems ()
+    {
+        return items;
+    }
+
+    public void setItems (Items[] items)
+    {
+        this.items = items;
+    }
+}
---
+++ app/src/main/java/com/example/phoebemanning/capstone/Models/Image_Models/Items.java
@@ -0,0 +1,14 @@
+package com.example.phoebemanning.capstone.Models.Image_Models;
+
+public class Items {
+
+    String[] images;
+
+    public String[] getImages() {
+        return images;
+    }
+
+    public void setImages(String[] images) {
+        this.images = images;
+    }
+}
