f8f01da33216c2e6fc2f47901f0ee2a16f6ce964
Phoebe <phoebe.manning8@gmail.com>
2018-07-05 11:36:53 -0700

    adds recyclerview adapter class

---
+++ app/src/main/java/com/example/phoebemanning/capstone/RecyclerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.phoebemanning.capstone;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private List<String> nutrientTitles;
+    private Context context;
+
+    public RecyclerAdapter(List<String> nutrientTitles, Context context){
+        this.nutrientTitles = nutrientTitles;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_layout, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder viewHolder, int i) {
+        String title = nutrientTitles.get(i);
+        viewHolder.Title.setText(title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return nutrientTitles.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Title;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Title = itemView.findViewById(R.id.nutrient_item);
+        }
+    }
+}
